fde08bf5b71b5e1c351c978af57d3c7e417e4303
Sean Owiecki <sowiecki@walmartlabs.com>
2015-07-28 00:13:43 -0500

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require("gulp");
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var babelify = require("babelify");
+var browserify = require('browserify');
+var eslint = require('gulp-eslint');
+
+var path = 'src/**/*',
+    application = './src/application.jsx';
+
+gulp.task("prod", ['lint'], function () {
+  browserify({
+      entries: application,
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('application.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task("dev", function() {
+  browserify({
+      entries: application,
+      debug: true
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('application.js'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('lint', function () {
+  return gulp.src([path])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task('watch', ['dev'], function(){
+	gulp.watch(path, ['dev'])
+});
