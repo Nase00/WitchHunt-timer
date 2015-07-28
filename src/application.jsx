3407bd4cef8d57ac3d96eaf6315eac388237e979
Sean Owiecki <sowiecki@walmartlabs.com>
2015-07-28 00:53:23 -0500

    Add basic timer.

--- src/application.jsx
+++ src/application.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React from 'react/addons';
+import moment from 'moment';
+
+let timeLeft = (seconds) => {
+	return moment(seconds * 1000).format('mm:ss');
+};
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			timer: 300,
+			day: 1,
+			sleeping: false,
+			running: false
+		};
+		this.onClick = this.onClick.bind(this);
+		this.countdown = this.countdown.bind(this);
+	}
+	componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+  countdown() {
+  	if (this.state.timer > 0) {
+	  	this.setState({timer: this.state.timer - 1});
+	  } else {
+	  	clearInterval(this.interval)
+	  }
+  }
+	onClick() {
+		if (this.state.running) {
+			clearInterval(this.interval)
+		} else {
+			this.interval = setInterval(this.countdown, 100);
+		}
+	}
 	render() {
+		let dayNightIndicator = this.state.sleeping ? "Night" : "Day";
 		return (
 			<div>
-				
+				<div className='day-night-indicator'>
+					{dayNightIndicator + this.state.day}
+				</div>
+				<div className='timer'>
+					{timeLeft(this.state.timer)}
+				</div>
+				<div className='timer-control'>
+					<button
+						className='button-begin'
+						value='start'
+						onClick={this.onClick}>
+						Begin Day
+					</button>
+				</div>
 			</div>
 		);
 	}
